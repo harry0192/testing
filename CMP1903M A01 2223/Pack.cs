9da5a4349dca1c82f33a80e85e756a41bf1806bc
harry0192 <72192076+harry0192@users.noreply.github.com>
2023-03-07 22:54:15 +0000

    Add files via upload

---
+++ CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack;
+
+        public Pack()
+        {
+            //Initialise the card pack here
+        }
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+            if (typeOfShuffle == 1) 
+            {
+                //Fisher-Yates shuffle
+            }
+            else if (typeOfShuffle == 2)
+            {
+                //Riffle shuffle
+            }
+            else if (typeOfShuffle == 3)
+            {
+                //No shuffle
+            }
+        }
+        public static Card deal()
+        {
+            //Deals one card
+
+        }
+        public static List<Card> dealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+        }
+    }
+}
